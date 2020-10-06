5fb719da7a93a37ab1029801e853c6b956ee1c7b
derfesl <derfesl@outlook.com>
2020-10-07 00:08:53 +0200

    debug feature

--- api/db.php
+++ api/db.php
@@ -9,7 +9,12 @@ $db = mysqli_connect($sql_host, $sql_username, $sql_password, $sql_dbname);
 if(!$db) exit('Database connection error: '.mysqli_connect_error());
 
 // simple query functions
-function query($sql) { global $db; return mysqli_query($db, $sql); }
+function query($sql) { 
+	global $db;
+	$result = mysqli_query($db, $sql);
+	if (!$result) { http_error(500, mysqli_error($db)); }
+	return $result;
+}
 function q_firstField($sql) { return mysqli_fetch_row(query($sql))[0]; }
 function q_firstRow($sql) { return mysqli_fetch_array(query($sql), MYSQLI_ASSOC); }
 function q_firstColumn($sql) { return array_map(function ($a) { return $a[0]; }, mysqli_fetch_all(query($sql))); }
