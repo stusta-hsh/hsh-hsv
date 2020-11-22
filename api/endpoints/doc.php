51742d6d018d8d9d7ddaaab6d8247e495732795b
derfesl <derfesl@outlook.com>
2020-11-22 12:18:25 +0100

    function returning function doc

--- api/endpoints/doc.php
+++ api/endpoints/doc.php
@@ -7,7 +7,7 @@ switch ($_GET['q']) {
 	case 'endpoints': output(endpoints()); break;
 	case 'functions': output(functions()); break;
 	case 'doc': output(doc()); break;
-	default: break;
+	default: http_error(400, "the requested function \"$_GET[q]\" doesn't exist"); break;
 }
 
 // --------------
@@ -30,7 +30,25 @@ function endpoints() {
 }
 
 function functions() {
+	$endpoint = require_param($_GET['endpoint']);
+	$file = file("../docs/$endpoint.md");
+	if (!$file) { http_error(400, "The endpoint \"$endpoint\" doesn't exist."); }
 
+	$i = 0;
+	$functions = array();
+	while (substr($file[$i], 0, 11) != '## Overview') { $i++; }
+	$i++;
+
+	while (substr($file[$i], 0, 1) == '*') {
+		$colon = strpos($file[$i], ':');
+		array_push($functions, array(
+			'name' => substr($file[$i], 3, $colon - 4),
+			'desc' => substr($file[$i], $colon + 2, -1)
+		));
+		$i++;
+	}
+
+	return $functions;
 }
 
 function doc() {
